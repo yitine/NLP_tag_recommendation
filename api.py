2493b8235c3a8ac4e2a9f684bda8f460b24776ed
Yi-Ting Chen <yi-tingchen@Yi-Tings-MBP.lan>
2023-07-19 15:43:49 +0200

    modify app

--- api.py
+++ api.py
@@ -3,20 +3,20 @@ from joblib import load
 from preprocess import preprocess, clean_output
 import sklearn
 
-app = Flask(__name__)
+api = Flask(__name__)
 
 pipe = load('OneVs_logReg.joblib')
 pipe2 = load('countVectorizer.joblib')
 
 
 # route test hello world
-@app.route("/")
+@api.route("/")
 def hello():
     return "Hello World!"
 
 
 # route api pour requête get
-@app.route("/api/text=<text>")
+@api.route("/api/text=<text>")
 def my_api(text) :
 	text_clean = preprocess(text)
 	features = pipe2.transform([text_clean])
@@ -31,7 +31,7 @@ def my_api(text) :
 	return jsonify(dictionnaire)
 
 if __name__ == "__main__" :
-	app.run(debug = True)
+	api.run(debug = True)
 
 
 # http://127.0.0.1:5000/api/text=python
